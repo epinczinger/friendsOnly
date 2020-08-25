3def446b998135e4b9570d0b80de037c4a625d87
umairarshadbutt <umair6622@gmail.com>
2020-08-26 01:06:09 +0500

    integrate liners, and fix rubocoperrors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,22 +1,20 @@
 class PostsController < ApplicationController
   def new
     @post = Post.new
-end
+  end
 
-def create
+  def create
     @post = Post.new(post_params)
     @post.user_id = current_user.id
     @post.save
     redirect_to root_path
   end
 
-
   def index
     @posts = Post.all
   end
 
-
-def post_params
-params.require(:post).permit(:title, :body)
-end
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
 end

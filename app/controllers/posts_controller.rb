49143dc21192bc14bb6e806e53614dc1beeb5908
EstebanP <epinczinger@gmail.com>
2020-08-25 16:45:56 -0300

    new and index views done, post_controller and user-post associations

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,22 @@
 class PostsController < ApplicationController
-    def new
-        @post = Post.new
-      end
-      
+  def new
+    @post = Post.new
+end
+
+def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    @post.save
+    redirect_to root_path
+  end
+
+
+  def index
+    @posts = Post.all
+  end
+
+
+def post_params
+params.require(:post).permit(:title, :body)
+end
 end

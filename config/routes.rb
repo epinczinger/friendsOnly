d9d88b28360f5f3ea559392e19759d1e8fe3948d
umairarshadbutt <umair6622@gmail.com>
2020-08-26 00:34:02 +0500

    Post controller and model generated

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :posts, only: [:new, :create, :index]
+  root 'posts#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
